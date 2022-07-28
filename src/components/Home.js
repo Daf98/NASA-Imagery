c8e97e95194162a6e039b18b8e5ee9b19cd5faa4
Daf <dafne.azzolina@gmail.com>
2022-07-28 16:51:20 -0300

    Add filter to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+
 import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -13,16 +15,32 @@ const Home = () => {
     dispatch(fetchDataFromAPI());
   }, [dispatch]);
 
+  const filterCategories = () => {
+    const homeBtn = document.querySelectorAll('.home-btn');
+    const searchBar = document.getElementById('search-bar');
+    homeBtn.forEach((homeButton) => {
+      const inputText = homeButton.childNodes[0].childNodes[0].textContent;
+      const lowerCase = inputText.charAt(0).toLowerCase();
+      if (lowerCase.match(searchBar.value[0])) {
+        homeButton.style.display = 'block';
+      } else {
+        homeButton.style.display = 'none';
+      }
+    });
+  };
+
   return (
     <>
       <Navbar />
       <div id="homepage">
         <h1 id="home-header">CELESTIAL BODIES</h1>
         <h2 id="home-sub">STATS BY CELESTIAL BODY</h2>
+        <input type="search" id="search-bar" placeholder="Search..." onChange={filterCategories} />
         <div id="home-grid">
           {celestialBodies.map((body) => (
             <button
               id="home-btn"
+              className="home-btn"
               type="button"
               key={body.collection.items[0].data[0].date_created}
             >
