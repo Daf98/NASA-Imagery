8aee21ea69e0f9c4001bc0be1c9734c97451c8af
Daf <dafne.azzolina@gmail.com>
2022-07-28 14:18:17 -0300

    Finish integration testing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,62 +1,30 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import App from "../App";
-import Home from "../components/Home";
-import Filter from '../components/Filter';
-import store from "../redux/configureStore";
-import "@testing-library/jest-dom";
-import { MemoryRouter } from "react-router-dom";
-import Category from "../components/Category";
-import Navbar from "../components/Navbar";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, screen, waitFor } from '@testing-library/react';
+import App from '../App';
+import store from '../redux/configureStore';
+import '@testing-library/jest-dom';
 
 jest.setTimeout(9000);
 
-const bodies = ["Planets", "Black-holes", "Nebulas", "Suns", "Galaxies"];
-
-describe("Jest Snapshot testing suite", () => {
-  it("Matches DOM Snapshot", () => {
+describe('Jest Snapshot testing suite', () => {
+  it('Matches DOM Snapshot', () => {
     const { container } = render(
       <Provider store={store}>
         <App />
-      </Provider>
+      </Provider>,
     );
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  it("should return the path from Planets", async () => {
-    for (let i = 0; i < bodies.length; i += 1) {
-      await fetch(`https://images-api.nasa.gov/search?q=planet`);
-    }
+  it('should return the path from Planets', async () => {
     render(
-      // <MemoryRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-        // </MemoryRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>,
     );
-
-    await waitFor(() => {
-      fireEvent.click(screen.getByText("Planets"));
-      expect(window.location.pathname).toBe("/detail/Planets");
-    });
-  });
-
-  it("should return the path from Planets", async () => {
-    for (let i = 0; i < bodies.length; i += 1) {
-      await fetch(`https://images-api.nasa.gov/search?q=planet`);
-    }
-    render(
-      // <MemoryRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-        // </MemoryRouter>
-    );
-
-    await waitFor(() => {
-      fireEvent.click(screen.getByText("Nebulas"));
-      expect(window.location.pathname).toBe("/detail/Nebulas");
+    waitFor(() => {
+      expect(screen.findByText('CELESTIAL').toBeInTheDocument());
     });
   });
 });
